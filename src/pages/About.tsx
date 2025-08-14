1797af04cfcd1db40260a1a8c9f1b2a199b9d7e2
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2025-08-15 05:00:16 +0900

    add link for about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,10 +2,10 @@ export default function About() {
   return (
     <div className="flex flex-col justify-center items-center bg-white h-screen -mt-16 -mb-12">
       <pre className="lg:text-5xl md:text-4xl sm:text-3xl text-2xl font-mono text-center leading-snug">
-        {`┌ me ┐ ┌ web ┐
-takagi@ytkg.jp
-│     └ x ┘  │
-└─── mail ───┘`}
+        ┌ me ┐ ┌ <a href="https://ytkg.jp" target="_blank" rel="noreferrer">web</a> ┐<br />
+        takagi@ytkg.jp<br />
+        │     └ <a href="https://x.com/ytkg_" target="_blank" rel="noreferrer">x</a> ┘  │<br />
+        └─── <a href="mailto:takagi@ytkg.jp">mail</a> ───┘
       </pre>
     </div>
   );
